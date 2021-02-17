00c99884e3ed34311cf3d08b2fecdb1dfc075a9e
unknown <daniel.camposdev@gmail.com>
2021-02-16 22:01:11 -0400

    Alter message

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export const handler = async (): Promise<any> => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify("Hi from aws lambda")
+        body: JSON.stringify("Hi aws lambda")
     };
     return response;
 };
